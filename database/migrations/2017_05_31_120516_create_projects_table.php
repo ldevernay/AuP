435c48afa5b68704f4b6048553538d11ff8b7fc3
ldevernay <boblehamster@yahoo.fr>
2017-05-31 14:09:11 +0200

    migration projects

---
+++ database/migrations/2017_05_31_120516_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+  public function up()
+  {
+    Schema::create('projects', function(Blueprint $table) {
+    $table->increments('id');
+    $table->timestamps();
+    $table->string('name', 80);
+    $table->text('pitch');
+    $table->string('github', 80);
+    $table->integer('creator_id')->unsigned();
+    $table->foreign('creator_id')
+        ->references('id')
+        ->on('users')
+        ->onDelete('restrict')
+        ->onUpdate('restrict');
+  });
+}
+
+public function down()
+{
+  Schema::table('projects', function(Blueprint $table) {
+    $table->dropForeign('projects_user_id_foreign');
+  });
+  Schema::drop('projects');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::resource('user', 'UserController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
